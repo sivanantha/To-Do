d7fa385bb89dbaefd55b76ac2b7560ca05af2525
sivanantham <sivanantham.chinnaiyan@ideas2it.com>
2021-12-10 00:24:08 +0530

    completed left side of to do

--- to-do.js
+++ to-do.js
@@ -1,8 +1,13 @@
 let sidebar = document.querySelector(".sidebar-left");
 let sidebarOpenButton = document.getElementById("sidebar-open-btn");
 
+(function() {
+    let today = new Date().toLocaleDateString("en-us",{weekday: "long", month: "long", day:"numeric"});
+    document.querySelector(".date-container").innerHTML = today;
+})();
+
 function openSideBar() {
-    sidebar.style.width = "25%";
+    sidebar.style.width = "27%";
     sidebarOpenButton.style.display = "none";
 
 }
