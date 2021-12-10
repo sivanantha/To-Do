68a62c874ac841abb853ddd96b1f9dd00ae6942e
sivanantham <sivanantham.chinnaiyan@ideas2it.com>
2021-12-11 00:35:25 +0530

    Added basic javascript functions

--- to-do.js
+++ to-do.js
@@ -1,18 +1,46 @@
-let sidebar = document.querySelector(".sidebar-left");
-let sidebarOpenButton = document.getElementById("sidebar-open-btn");
+"use strict";
 
-(function() {
-    let today = new Date().toLocaleDateString("en-us",{weekday: "long", month: "long", day:"numeric"});
-    document.querySelector(".date-container").innerHTML = today;
-})();
+(function () {
+  let dateContainer = document.querySelector(".date-container");
+  let newListInput = document.querySelector(".new-list-container input");
+  let sidebar = document.querySelector(".sidebar-left");
+  let sidebarOpenButton = document.getElementById("sidebar-open-btn");
+  let sidebarCloseButton = document.getElementById("sidebar-close-btn");
 
-function openSideBar() {
+  function init() {
+    bindEventListeners();
+    setTodayDate();
+  }
+
+  function bindEventListeners() {
+    addListener(sidebarOpenButton, "click", openSideBar);
+    addListener(sidebarCloseButton, "click", closeSideBar);
+    addListener(newListInput, "keyup", createList);
+  }
+
+  function addListener(element, event, listener) {
+    element.addEventListener(event, listener);
+  }
+
+  function openSideBar() {
     sidebar.style.width = "21vw";
     sidebarOpenButton.style.display = "none";
+  }
 
-}
-
-function closeSideBar() {
+  function closeSideBar() {
     sidebar.style.width = "0px";
     sidebarOpenButton.style.display = "inline-block";
-}
+  }
+
+  function setTodayDate() {
+    dateContainer.innerHTML = new Date().toLocaleDateString("en-us", {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+    });
+  }
+
+  function createList() {}
+
+  init();
+})();
