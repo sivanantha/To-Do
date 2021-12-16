80eefbd3b947a447a3458f1d935e642c231e2b30
sivanantham <sivanantham.chinnaiyan@ideas2it.com>
2021-12-16 13:30:34 +0530

    stable: Added all features, doc comments. cleaned the code.

--- to-do.js
+++ to-do.js
@@ -8,14 +8,12 @@
     { name: "Assigned to me", icon: "ms-Icon--Contact", tasks: [] },
     { name: "Tasks", icon: "ms-Icon--Home", tasks: [] },
   ];
-  const createListContainer = ".newlist-container";
-  const myDay = "#my-day";
-  const important = "#important";
-  const listContainer = ".lists";
-  const taskCheckbox = ".tickbox-container";
   let currentPage = "My Day";
   let currentTask = "";
 
+  /**
+   * Triggers the startup functions and intializes the program.
+   */
   function init() {
     getSnippet("category-snippet", renderDefaultCategories);
     getSnippet("middle-column-snippet", renderMiddlePage);
@@ -25,20 +23,40 @@
     bindEventListener(".add-steps input", "keyup", createStep);
   }
 
+  /**
+   * Adds event listener to the specified element.
+   *
+   * @param element the target element selector to add event listener.
+   * @param event the name of the event.
+   * @param listener the function to be called if event is triggered.
+   */
   function bindEventListener(element, event, listener) {
-    if (element instanceof window.Element) {
-      element.addEventListener(event, listener);
-    } else {
-      document.querySelector(element).addEventListener(event, listener);
-    }
+    document.querySelector(element).addEventListener(event, listener);
   }
 
+  /**
+   * Adds event listener to all the specified elements.
+   *
+   * @param elements the target element selector to add event listener.
+   * @param event the name of the event.
+   * @param listener the function to be called if event is triggered.
+   */
   function bindEventListenerToAll(elements, event, listener) {
-    for (let element of elements) {
-      element.addEventListener(event, listener);
+    let nodes = document.querySelectorAll(elements);
+    for (let node of nodes) {
+      node.addEventListener(event, listener);
     }
   }
 
+  /**
+   * Fetches the specified snippet from the server. When the response
+   * is received invokes the callback function and passes the response
+   * as the parameter.
+   *
+   * @param snippetName the snippet to be fetched.
+   * @param handler the callback function to invoked when response
+   *        is received.
+   */
   function getSnippet(snippetName, handler) {
     let request = new XMLHttpRequest();
     request.open("GET", `snippets/${snippetName}.html`, true);
@@ -49,16 +67,33 @@
     request.send();
   }
 
-  function insertHtml(parent, html) {
-    document.querySelector(parent).innerHTML = html;
+  /**
+   * Inserts the specified html into the target element.
+   * 
+   * @param targetElement the element in which the html to be inserted.
+   * @param html the html to be inserted.
+   */
+  function insertHtml(targetElement, html) {
+    document.querySelector(targetElement).innerHTML = html;
   }
 
+  /**
+   * Inserts the specified property in the given html.
+   * 
+   * @param html the html in which property to be inserted.
+   * @param propertyName the name of the property.
+   * @param propertyValue the value of the property to insert.
+   * @returns the html with inserted property value.
+   */
   function insertProperty(html, propertyName, propertyValue) {
     var propertyToReplace = "{{" + propertyName + "}}";
     html = html.replace(new RegExp(propertyToReplace, "g"), propertyValue);
     return html;
   }
 
+  /**
+   * Toggles (open/close) the left sidebar.
+   */
   function toggleSideBar() {
     let node = document.querySelector(".left-column");
     node.classList.toggle("open-sidebar");
@@ -67,6 +102,12 @@
       .classList.toggle("hide-element");
   }
 
+  /**
+   * Gets the today date in the following format.
+   * (e.g) Thursday, December 16
+   * 
+   * @returns the today date.
+   */
   function getTodayDate() {
     return new Date().toLocaleDateString("en-us", {
       weekday: "long",
@@ -75,14 +116,29 @@
     });
   }
 
+  /**
+   * Renders the default categories.
+   * (My Day, Important, Planned, Assigned To Me, Tasks)
+   * 
+   * @param templateHtml the categories template html to render
+   *        categories. 
+   */
   function renderDefaultCategories(templateHtml) {
     renderCategories(templateHtml, categories, ".default-categories");
-    bindEventListenerToAll(document.querySelectorAll(".default-categories a"), "click", changeCategory);
+    bindEventListenerToAll(".default-categories a", "click", changeCategory);
   }
 
+  /**
+   * Renders the categories from the specified categories array.
+   * 
+   * @param templateHtml the categories template html to render
+   *        categories.
+   * @param categoryList the array containing categories.
+   * @param targetElement the elemment in which the html to be inserted.
+   */
   function renderCategories(
     templateHtml,
-    categoryList = categories.slice(5,),
+    categoryList = categories.slice(5),
     targetElement = ".category-container"
   ) {
     let html = "";
@@ -92,9 +148,16 @@
       html += insertProperty(temp, "icon", category.icon);
     }
     insertHtml(targetElement, html);
-    bindEventListenerToAll(document.querySelectorAll(".category-container a"), "click", changeCategory);
+    bindEventListenerToAll(".category-container a", "click", changeCategory);
   }
 
+  /**
+   * Handles the create category input event.
+   * Creates a new category and stores in the category array.
+   * Invokes the renderCategories and renderTasks functions.
+   * 
+   * @param event the event object passed by the event handler. 
+   */
   function createCategory(event) {
     let categoryName = event.target.value;
     if (event.keyCode === 13 && categoryName != "") {
@@ -106,22 +169,34 @@
       currentPage = categoryName;
       getSnippet("category-snippet", renderCategories);
       getSnippet("middle-column-snippet", renderTasks);
-      changeCategory(event);
+      changePageTitle();
       event.target.value = "";
     }
   }
 
+  /**
+   * Handles the create task input event.
+   * Creates a new task and stores in the current category.
+   * Invokes the renderTasks function.
+   * 
+   * @param event the event object passed by the event handler.
+   */
   function createTask(event) {
-    if (event.keyCode === 13 && event.target.value != "") {
-      let taskInput = document.querySelector(
-        ".task-list-container .add-task input"
-      );
-      getTasks(currentPage).push({ name: taskInput.value, steps: [] });
+    let taskInput = event.target.value;
+    if (event.keyCode === 13 && taskInput != "") {
+      getTasks(currentPage).push({ name: taskInput, steps: [] });
       getSnippet("task-snippet", renderTasks);
-      taskInput.value = "";
+      event.target.value = "";
     }
   }
 
+  /**
+   * Fetches the specified categories tasks.
+   * 
+   * @param categoryName the category name whose tasks to be fetched.
+   * @returns the array containing tasks if category is found, otherwise an 
+   *          empty array.
+   */
   function getTasks(categoryName) {
     for (let category of categories) {
       if (category.name === categoryName) {
@@ -131,6 +206,12 @@
     return [];
   }
 
+  /**
+   * Renders the tasks of the current category.
+   * 
+   * @param templateHtml the task template html to render
+   *        tasks.
+   */
   function renderTasks(templateHtml) {
     let html = "";
     let temp;
@@ -139,16 +220,21 @@
       html = insertProperty(temp, "stepCount", task.steps.length) + html;
     }
     insertHtml(".tasks", html);
-    let checkboxes = document.querySelectorAll(".tasks .tickbox-container");
-    console.log(document.querySelectorAll(".task-name"));
+    bindEventListenerToAll("span.task-name", "click", toggleTaskPanel);
     bindEventListenerToAll(
-      document.querySelectorAll("span.task-name"),
+      ".tasks .tickbox-container",
       "click",
-      toggleTaskPanel
+      toggleCheckboxAndStrikeThrough
     );
-    bindEventListenerToAll(checkboxes, "click", markAsCompleted);
   }
 
+  /**
+   * Fetches the current categoriy's specified task's steps.
+   * 
+   * @param taskName the task name whose steps to be fetched.
+   * @returns the array containing steps if task is found, otherwise an 
+   *          empty array.
+   */
   function getSteps(taskName) {
     for (let task of getTasks(currentPage)) {
       if (task.name === taskName) {
@@ -158,16 +244,30 @@
     return [];
   }
 
-  function createStep(key) {
-    if (key.code === "Enter") {
-      let stepInput = document.querySelector(".add-steps input");
+  /**
+   * Handles the create step input event.
+   * Creates a new step and stores in the current task.
+   * Invokes the renderSteps function.
+   * 
+   * @param event the event object passed by the event handler.
+   */
+  function createStep(event) {
+    let stepInput = event.target.value;
+    if (event.code === "Enter" && stepInput != "") {
       let steps = getSteps(currentTask);
-      steps.push(stepInput.value);
+      steps.push(stepInput);
       getSnippet("step-snippet", renderSteps);
-      stepInput.value = "";
+      event.target.value = "";
     }
   }
 
+  /**
+   * Renders the steps of the current task.
+   * Invokes the rederTasks function.
+   * 
+   * @param templateHtml the step template html to render
+   *        steps.
+   */
   function renderSteps(templateHtml) {
     let html = "";
     for (let step of getSteps(currentTask)) {
@@ -175,13 +275,19 @@
     }
     insertHtml(".steps-container", html);
     bindEventListenerToAll(
-      document.querySelectorAll(".task-details button.tickbox-container"),
+      ".task-details button.tickbox-container",
       "click",
-      markAsCompleted
+      toggleCheckboxAndStrikeThrough
     );
     getSnippet("task-snippet", renderTasks);
   }
 
+  /**
+   * Toggles (open/close) the right task details panel.
+   * Invokes the renderSteps function.
+   * 
+   * @param event the event object passed by the event handler.
+   */
   function toggleTaskPanel(event) {
     let taskName = event.target.innerText;
     currentTask = taskName;
@@ -191,6 +297,12 @@
     panel.classList.toggle("open-task-panel");
   }
 
+  /**
+   * Renders the middle page.
+   * 
+   * @param templateHtml the middle page template html to render
+   *        middle page.
+   */
   function renderMiddlePage(templateHtml) {
     let html = insertProperty(templateHtml, "title", "My Day");
     insertHtml(".middle-column", html);
@@ -203,21 +315,48 @@
     );
   }
 
+  /**
+   * Handles the categories on click event.
+   * Changes the current category. Invokes the renderTasks to load
+   * selected categoriy's tasks.
+   * 
+   * @param event the event object passed by the event handler.
+   */
   function changeCategory(event) {
     currentPage = event.target.innerText;
+    getSnippet("task-snippet", renderTasks);
+    changePageTitle();
+  }
+
+  /**
+   * Changes the page title to the selected category name.
+   * If selected category is My Day then displays date.
+   * Category names are displayed in blue color except My Day.
+   */
+  function changePageTitle() {
     let pageTitle = document.querySelector(".toolbar span.toolbar-title");
     pageTitle.innerText = currentPage;
-    getSnippet("task-snippet", renderTasks);
     if (currentPage === "My Day") {
-      document.querySelector(".toolbar span.date-container").classList.remove("hide-element");
+      document
+        .querySelector(".toolbar span.date-container")
+        .classList.remove("hide-element");
       pageTitle.classList.remove("color-blue");
     } else {
-      document.querySelector(".toolbar span.date-container").classList.add("hide-element");
+      document
+        .querySelector(".toolbar span.date-container")
+        .classList.add("hide-element");
       pageTitle.classList.add("color-blue");
     }
   }
 
-  function markAsCompleted(event) {
+  /**
+   * Handles the task and step checkbox on click events.
+   * Toggles the checkbox and strike througth the text of the task 
+   * or step element which triggered the event.
+   * 
+   * @param {*} event the event object passed by the event handler.
+   */
+  function toggleCheckboxAndStrikeThrough(event) {
     let targetElement = event.target;
     if (targetElement.matches("i")) {
       targetElement.parentNode.children[0].classList.toggle(
@@ -230,4 +369,5 @@
   }
 
   init();
+  
 })();
